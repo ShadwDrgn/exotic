7e32a2ff67534713e01a86d0d1017eab74da5b61
ShadwDrgn <c.arcane1@gmail.com>
2023-06-07 15:29:24 -0400

    hack in some login stuff

--- app.py
+++ app.py
@@ -1,6 +1,63 @@
 from classes import *
 from flask import Flask, current_app, jsonify, request, abort
+import flask_login
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+app.secret_key = 'giant stupid string that is definitely at total secret!!'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+users = {
+    'ShadwDrgn': {'password': 'pbkdf2:sha256:600000$OtUeSwJ3spOhzAjf$fda51497ebebb9b82914332c8bd22aaffb9f536565201f212af1ae8848a7caa1'}
+}
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    return user
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+            <form action='login' method='POST'>
+            <input type='text' name='username' id='username' placeholder='username'/>
+            <input type='password' name='password' id='password' placeholder='password'/>
+            <input type='submit' name='submit'/>
+            </form>
+            '''
+    username = request.form['username']
+    if username in users and check_password_hash(users[username]['password'], request.form['password']):
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return jsonify({'message': 'logged in successfully'})
+    return jsonify({'error': 'Bad Login'}), 401
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return '''
+            <form action='register' method='POST'>
+            <input type='text' name='username' id='username' placeholder='username'/>
+            <input type='password' name='password' id='password' placeholder='password'/>
+            <input type='submit' name='submit'/>
+            </form>
+            '''
+    username = request.form['username']
+    password_hash = generate_password_hash(request.form['password'])
+    if username in users:
+        return jsonify({'error': 'User already exists'}), 400
+    users[username] = {'password': password_hash}
+    return jsonify({'message': 'User registered successfully'})
+
 
 @app.route("/tiles")
 def tiles():
